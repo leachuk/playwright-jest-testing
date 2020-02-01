b40fb7dbe0a263ec50a8e01aee7900956d3957db
stewart leach <stew.leach@gmail.com>
2020-02-01 22:15:43 +1100

    adding swinburne screenshot example test

--- playwright.test.js
+++ playwright.test.js
@@ -3,7 +3,7 @@ const timeout = 5000
 describe(
 	'/ (Home Page)',
 	() => {
-		let page
+		let page;
 		beforeAll(async () => {
 			let browser = await global.__BROWSER__;
 			let context = await browser.newContext({
@@ -14,21 +14,21 @@ describe(
 				}
 			});
 			page = await context.newPage('https://google.com')
-		}, timeout)
+		}, timeout);
 
 		afterAll(async () => {
 			await page.close()
-		})
+		});
 
 		it('should load without error', async () => {
 			let text = await page.evaluate(() => document.body.textContent)
 			expect(text).toContain('google')
-		})
+		});
 
-		it('should load without error', async () => {
+		it('should also load without error', async () => {
 			let text = await page.evaluate(() => document.body.textContent)
 			expect(text).not.toContain('googlefoo')
-		})
+		});
 	},
 	timeout
-)
+);
---
+++ swinburne.test.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const timeout = 5000;
+
+describe(
+	'/ (Home Page)',
+	() => {
+		let page;
+		let screenshotDir = './screenshots';
+		beforeAll(async () => {
+			if (!fs.existsSync(screenshotDir)){
+				fs.mkdirSync(screenshotDir);
+			}
+
+			let browser = await global.__BROWSER__;
+			let context = await browser.newContext({
+				viewport: {
+					width: 1000,
+					height: 600,
+					deviceScaleFactor : 1
+				}
+			});
+			page = await context.newPage('https://www.swinburne.edu.au/study/life/why-choose-swinburne/')
+		}, timeout)
+
+		afterAll(async () => {
+			await page.close()
+		})
+
+		it('should load without error', async () => {
+			const element = await page.$("body > div.l-wrapper.l-wrapper--main > section");
+			await element.screenshot({ path: screenshotDir + `/` + `swinburnetest.png` });
+			expect(true);//temp force success
+		})
+	},
+	timeout
+)
