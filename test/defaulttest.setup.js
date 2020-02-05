3e3e3fceab7f55d198f2d3c85ca62bec91d7aa2a
stewart leach <stew.leach@gmail.com>
2020-02-05 22:16:36 +1100

    WIP setting up test defaults and rendition configs

--- test/defaulttest.setup.js
+++ test/defaulttest.setup.js
@@ -1,16 +1,29 @@
 class DefaultTestSetup {
     constructor() {
-        this.playwright = {};
-        this.playwright.context = {
-            viewport: {
-                width: 1000,
-                height: 600,
-                deviceScaleFactor: 1
+        this.browser = {
+            renditions: [
+                {label: "SM", width: 320, height: 480},
+                {label: "MD", width: 640, height: 768},
+                {label: "SM", width: 1024, height: 768},
+                {label: "SM", width: 1366, height: 1280}
+            ]
+        };
+
+        this.playwright = {
+            context: {
+                viewport: {
+                    width: 1000,
+                    height: 600,
+                    deviceScaleFactor: 1
+                }
             }
         };
     }
     get browserContext() {
         return this.playwright.context;
+    }
+    get browserRenditions() {
+        return this.browser.renditions;
     }
 }
 
--- test/specs/component/list/pagelist/pagelist.screenshot.test.js
+++ test/specs/component/list/pagelist/pagelist.screenshot.test.js
@@ -9,19 +9,40 @@ describe(
 
 		beforeAll(async () => {
 			let browser = await global.__BROWSER__;
-			let context = await browser.newContext(testConfig.browserContext);
-			page = await context.newPage('https://www.swinburne.edu.au/study/life/why-choose-swinburne/')
+			//let context = await browser.newContext(testConfig.browserContext);
+			//page = await context.newPage('https://www.swinburne.edu.au/study/life/why-choose-swinburne/')
 		}, timeout);
 
 		afterAll(async () => {
 			await page.close()
 		});
 
-		it('should load without error', async () => {
-			const element = await page.$("body > div.l-wrapper.l-wrapper--main > section");
-			const image = await element.screenshot();
-			expect(image).toMatchImageSnapshot();//temp force success
-		})
+		test('adds 1 + 2 to equal 3', () => {
+			console.log(testConfig.browserRenditions[0].label)
+			expect(3).toBe(3);
+		});
+
+		test.each([testConfig.browserRenditions])(
+			'foo=%s, bar=%s, zoo=%s',
+			(a,b,c) => {
+			console.log('a:%o ,b:%o ,c:%o',a,b,c)
+			expect(true).toBe(true)
+		},
+		timeout
+		)
+		test.each([[1, 1, 2, {foo: "bar"}]])(
+			'.add(%i, %i)',
+			(a, b, expected, foo) => {
+				console.log('a:%i, b:%i, expected:%i, foo:%o',a,b,expected,foo.foo)
+				expect(a + b).toBe(expected);
+			},
+		);
+
+		// it('should load without error', async () => {
+		// 	const element = await page.$("body > div.l-wrapper.l-wrapper--main > section");
+		// 	const image = await element.screenshot();
+		// 	expect(image).toMatchImageSnapshot();
+		// })
 	},
 	timeout
 )
