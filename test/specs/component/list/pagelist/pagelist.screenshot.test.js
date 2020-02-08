d4bf5e0a0c7714a4297b9d03df94387a5bf7c6c9
Stewart Leach <stewart.leach@isobar.com>
2020-02-08 20:29:09 +1100

    resolving linting issues

--- test/specs/component/list/pagelist/pagelist.screenshot.test.js
+++ test/specs/component/list/pagelist/pagelist.screenshot.test.js
@@ -1,44 +1,47 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+const { username } = require('minimist')(process.argv.slice(2));
 const DefaultTestSetup = require('../../../../defaulttest.setup.js');
+
 const timeout = 5000;
-const username = require("minimist")(process.argv.slice(2))["username"];
 
 describe(
-	'/ (PageList Screenshot)',
-	() => {
-		let page;
-		let browserRenditions = new DefaultTestSetup().browserRenditions;
+  '/ (PageList Screenshot)',
+  () => {
+    let page;
+    const { browserRenditions } = new DefaultTestSetup();
 
-		let pagePath = "/content/swinburne-site-showcase/en/styleguide/components/page-list.html";
+    const pagePath = '/content/swinburne-site-showcase/en/styleguide/components/page-list.html';
 
-		beforeAll(async () => {
-			let browser = await global.__BROWSER__;
-			//page = await browser.newPage();
-			let context = await browser.newContext();
-			page = await context.newPage('https://www.swinburne.edu.au/study/life/why-choose-swinburne/')
-		}, timeout);
+    beforeAll(async () => {
+      const browser = await global.__BROWSER__;
+      // page = await browser.newPage();
+      const context = await browser.newContext();
+      page = await context.newPage('https://www.swinburne.edu.au/study/life/why-choose-swinburne/');
+    }, timeout);
 
-		afterAll(async () => {
-			await page.close()
-		});
+    afterAll(async () => {
+      await page.close();
+    });
 
-		test.each(browserRenditions.map(browserRenditions => [browserRenditions[0].label, browserRenditions[0]]))(
-			'Rendition test for size %s',
-			async(label,rendition) => {
-				console.log("custom cli param test %s", username);
-				console.log('label:%s ,height:%i ,width:%i',label,rendition.height,rendition.width);
-				await page.setViewport({
-					width: rendition.width,
-					height: rendition.height
-				});
+    test.each(browserRenditions.map((browserRenditions) => [browserRenditions[0].label, browserRenditions[0]]))(
+      'Rendition test for size %s',
+      async (label, rendition) => {
+        console.log('custom cli param test %s', username);
+        console.log('label:%s ,height:%i ,width:%i', label, rendition.height, rendition.width);
+        await page.setViewport({
+          width: rendition.width,
+          height: rendition.height,
+        });
 
-				const element = await page.$("body > div.l-wrapper.l-wrapper--main > section");
-				const image = await element.screenshot();
-				expect(image).toMatchImageSnapshot();
-				expect(true).toBe(true);
-			},
-		timeout
-		);
-
-	},
-	timeout
-)
+        const element = await page.$('body > div.l-wrapper.l-wrapper--main > section');
+        const image = await element.screenshot();
+        expect(image).toMatchImageSnapshot();
+        expect(true).toBe(true);
+      },
+      timeout,
+    );
+  },
+  timeout,
+);
